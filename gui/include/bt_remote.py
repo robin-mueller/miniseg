b40c00652a4ec706a111f7ad42e27e999e24bc6c
Robin Müller <robin-m-99@gmx.de>
2023-04-05 03:38:35 +0200

    Fix connecting and disconnecting

--- gui/include/bt_remote.py
+++ gui/include/bt_remote.py
@@ -1,9 +1,11 @@
 import json
 import time
-import bluetooth
 
+from bluetooth import discover_devices, BluetoothSocket
+from typing import Optional, Callable
 from threading import Lock
 from collections import UserDict
+from PySide6.QtCore import QThread, Signal, QObject
 
 
 class BTRemoteNotConnected(Exception):
@@ -11,13 +13,32 @@ class BTRemoteNotConnected(Exception):
 
 
 class BTRemote(UserDict):
+    class ConnectWorker(QObject):
+        connected = Signal()
+        connection_failed = Signal(str)
+        finished = Signal()
+        
+        def __init__(self, connect_handle: Callable[[], None]):
+            super().__init__()
+            self.connect_handle = connect_handle
+        
+        def run(self):
+            try:
+                self.connect_handle()
+                self.connected.emit()
+            except Exception as e:
+                self.connection_failed.emit(type(e).__name__)
+            finally:
+                self.finished.emit()
+        
     def __init__(self, address: str):
         super().__init__()
         self._address = address
         self._connect_lock = Lock()
         self._connected = False
-        self._socket = bluetooth.BluetoothSocket()
-        self._socket.settimeout(5)
+        self._socket: Optional[BluetoothSocket] = None
+        self._connect_worker: Optional[BTRemote.ConnectWorker] = None
+        self._connect_thread: Optional[QThread] = None
         
     def __setitem__(self, key, value):
         super().__setitem__(key, value)
@@ -26,13 +47,31 @@ class BTRemote(UserDict):
     def connect(self):
         with self._connect_lock:
             if not self._connected:
+                self._socket = BluetoothSocket()
+                self._socket.settimeout(5)
                 self._socket.connect((self._address, 1))
                 self._connected = True
+    
+    # noinspection PyUnresolvedReferences
+    def async_connect(self, on_connected_handle: Callable[[], None], on_connection_failed_handle: Callable[[str], None]):
+        self._connect_worker = BTRemote.ConnectWorker(self.connect)
+        self._connect_worker.connected.connect(on_connected_handle)
+        self._connect_worker.connection_failed.connect(on_connection_failed_handle)
+        
+        self._connect_thread = QThread()
+        self._connect_worker.moveToThread(self._connect_thread)
+        self._connect_thread.started.connect(self._connect_worker.run)
+        self._connect_worker.finished.connect(self._connect_thread.quit)
+        self._connect_worker.finished.connect(self._connect_worker.deleteLater)
+        self._connect_thread.finished.connect(self._connect_thread.deleteLater)
+        
+        self._connect_thread.start()
         
     def disconnect(self):
         with self._connect_lock:
             if self._connected:
                 self._socket.close()
+                self._socket = None
                 self._connected = False
         
     def publish_data(self):
@@ -48,7 +87,7 @@ class BTRemote(UserDict):
         
     @staticmethod
     def discover():
-        nearby_devices = bluetooth.discover_devices(duration=5, lookup_names=True)
+        nearby_devices = discover_devices(duration=5, lookup_names=True)
         print("Found {} devices.".format(len(nearby_devices)))
         
         for addr, name in nearby_devices:
--- gui/include/main_window.py
+++ gui/include/main_window.py
@@ -4,8 +4,9 @@ from gui.include.helper import MonitoringGraph, GraphDict
 from gui.include.curve_definition import CurveDefinition, CurveLibrary
 from gui.include.monitoring_window import MonitoringWindow
 from gui.include.widget import SetpointSlider, ParameterSection
+from PySide6.QtCore import Slot
 from PySide6.QtGui import QCloseEvent
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QProgressBar, QLabel
 
 
 # noinspection PyPropertyAccess
@@ -14,13 +15,18 @@ class MiniSegGUI(QMainWindow):
         self.monitors: list[MonitoringWindow] = []
         self.bt_remote = BTRemote("98:D3:A1:FD:34:63")
         
+        self.bt_connect_progress_bar = QProgressBar()
+        self.bt_connect_progress_bar.setMaximumSize(250, 15)
+        self.bt_connect_progress_bar.setRange(0, 0)
+        self.bt_connect_label = QLabel("Connecting ...")
+        
         super().__init__(None)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.plot_overview.setBackground(None)
-        self.ui.actionNewMonitor.triggered.connect(self.open_monitor)
-        self.ui.actionConnect.triggered.connect(self.bluetooth_connect)
-        self.ui.actionDisconnect.triggered.connect(self.bluetooth_disconnect)
+        self.ui.actionNewMonitor.triggered.connect(self.on_open_monitor)
+        self.ui.actionConnect.triggered.connect(self.on_bluetooth_connect)
+        self.ui.actionDisconnect.triggered.connect(self.on_bluetooth_disconnect)
         
         self.parameter_section = ParameterSection(self.ui.parameter_frame)
         self.setpoint_slider = SetpointSlider(self.ui.setpoint_slider_frame)
@@ -33,22 +39,38 @@ class MiniSegGUI(QMainWindow):
             title="Position [cm]"
         )
         self.graphs[0].start()
-        
-    def bluetooth_connect(self):
-        try:
-            self.ui.statusbar.showMessage("Connecting ...", 3000)
-            self.bt_remote.connect()
-            self.ui.actionConnect.setEnabled(False)
-            self.ui.actionDisconnect.setEnabled(True)
-        except Exception as e:
-            self.ui.statusbar.showMessage(f"Connecting failed - {type(e).__name__} occured!", 3000)
-            
-    def bluetooth_disconnect(self):
+    
+    @Slot()
+    def on_bluetooth_connect(self):
+        self.ui.actionConnect.setEnabled(False)
+        self.ui.statusbar.addWidget(self.bt_connect_label)
+        self.ui.statusbar.addWidget(self.bt_connect_progress_bar)
+        self.bt_connect_label.show()
+        self.bt_connect_progress_bar.show()
+        self.bt_remote.async_connect(self.on_bluetooth_connected, self.on_bluetooth_connection_failed)
+    
+    @Slot()
+    def on_bluetooth_connected(self):
+        self.ui.statusbar.removeWidget(self.bt_connect_label)
+        self.ui.statusbar.removeWidget(self.bt_connect_progress_bar)
+        self.ui.statusbar.showMessage("Connection successful!", 3000)
+        self.ui.actionDisconnect.setEnabled(True)
+    
+    @Slot(str)
+    def on_bluetooth_connection_failed(self, exception_name: str):
+        self.ui.statusbar.removeWidget()
+        self.ui.actionConnect.setEnabled(True)
+        self.ui.statusbar.showMessage(f"Connecting failed - {exception_name} occured!", 3000)
+    
+    @Slot()
+    def on_bluetooth_disconnect(self):
         self.bt_remote.disconnect()
         self.ui.actionConnect.setEnabled(True)
         self.ui.actionDisconnect.setEnabled(False)
+        self.ui.statusbar.showMessage("Disconnected from device!", 3000)
     
-    def open_monitor(self):
+    @Slot()
+    def on_open_monitor(self):
         new_monitor = MonitoringWindow()
         new_monitor.destroyed.connect(lambda: self.monitors.remove(new_monitor))
         self.monitors.append(new_monitor)
@@ -57,5 +79,6 @@ class MiniSegGUI(QMainWindow):
     def closeEvent(self, event: QCloseEvent):
         for monitor in self.monitors:
             monitor.close()
+        self.bt_remote.disconnect()
         super().closeEvent(event)
         
