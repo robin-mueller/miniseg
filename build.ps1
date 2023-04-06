c0876f27b6e4b6085e47becdaaa372859b054961
Robin Müller <robin-m-99@gmx.de>
2023-04-06 16:27:19 +0200

    Finish build

--- build.ps1
+++ build.ps1
@@ -4,14 +4,17 @@
 Set-Location $PSScriptRoot
 
 # Embed interface definitions as a resource for arduino compilation
-$InterfaceJsonContent = Get-Content -Path ".\interface.json" -Raw
-$InterfaceJsonContent = $InterfaceJsonContent | ForEach-Object {}
+$InterfaceJsonContent = Get-Content -Path ".\interface.json"
 $InterfaceHPath = ".\controller\interface.h"
-Set-Content -Path $InterfaceHPath -Value "#ifndef INTERFACE_H
+Set-Content -NoNewline -Path $InterfaceHPath -Value "#ifndef INTERFACE_H
 #define INTERFACE_H
 
-const char interface_def[] = `"$( $InterfaceJsonContent )`"
+const char interface_def[] = `"$([string]::Concat($InterfaceJsonContent.Trim().Replace(' ', '').Replace('"', '\"')) )`"
 
 #endif
 "
 
+# Generate gui resources
+Set-Location gui
+resources\generate.ps1
+Set-Location ..
--- controller/interface.h
+++ controller/interface.h
@@ -1,10 +1,6 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-const char interface_def[] = "{
-  "from_device": [],
-  "to_device": ["controller_state", {"A1":  ["B1", "B2"], "A2":  ["B3", "B4"]}, "A3"]
-}"
+const char interface_def[] = "{\"from_device\":[],\"to_device\":[\"controller_state\",{\"A1\":[\"B1\",\"B2\"],\"A2\":[\"B3\",\"B4\"]},\"A3\"]}"
 
 #endif
-
