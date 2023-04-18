4afac697a2c900a569b2d24464ec009736a25e32
Robin Müller <robin-m-99@gmx.de>
2023-04-18 17:01:30 +0200

    Upload working state

--- controller/src/communication/communication.cpp
+++ controller/src/communication/communication.cpp
@@ -15,7 +15,6 @@ Communication::Communication() {
 
 bool Communication::receive() {
   if (!Serial.available()) return false;
-  char buffer[RX_SERIAL_BUFFER_SIZE]{ 0 };
   union {
     uint16_t integer = 0;
     byte arr[2];
@@ -25,17 +24,17 @@ bool Communication::receive() {
   if (Serial.find(PACKET_START_TOKEN)) {
     // When packet start was found, read message length information from first two bytes
     Serial.readBytes(msg_len.arr, 2);
-    Serial.readBytes(buffer, msg_len.integer);
+    Serial.readBytes(RX_SERIAL_BUFFER, msg_len.integer);
 
     // Now read message
     StaticJsonDocument<JSON_DOC_SIZE_RX> rx_doc;
-    const DeserializationError err = deserializeJson(rx_doc, (const char *)buffer);  // prevent zero-copy mode since buffer should not be changed inplace as it is needed for the debug message.
+    const DeserializationError err = deserializeJson(rx_doc, (const char *)RX_SERIAL_BUFFER);  // prevent zero-copy mode since buffer should not be changed inplace as it is needed for the debug message.
     if (err) {
       message_clear();
       message_append(F("Deserialization ERROR: Failed with code: "));
       message_transmit(err.f_str());
       message_append(F("Tried to deserialize: "));
-      message_transmit(buffer);
+      message_transmit(RX_SERIAL_BUFFER);
       return false;
     }
     rx_data.from_doc(rx_doc);
@@ -44,7 +43,7 @@ bool Communication::receive() {
   return false;
 }
 
-bool Communication::transmit() {
+bool Communication::initiate_transmit() {
   StaticJsonDocument<JSON_DOC_SIZE_TX> tx_doc;
   tx_data.to_doc(tx_doc);
 
@@ -53,23 +52,42 @@ bool Communication::transmit() {
     message_transmit(F("Serialization ERROR: Document size for tx_doc too small!"));
     return false;
   } else {
-    write_packet(tx_doc);
+    tx_write_end_idx = build_packet(tx_doc, TX_SERIAL_BUFFER);
     return true;
   }
 }
 
-// Send a maximum of TX_SERIAL_BUFFER_SIZE bytes plus 3 bytes header (start char (1 byte) + message length information (2 bytes))
-void Communication::write_packet(JsonDocument &tx_doc) {
-  char buffer[TX_SERIAL_BUFFER_SIZE]{ 0 };
-  size_t msg_len = serializeJson(tx_doc, buffer);
+void Communication::async_transmit() {
+  if (tx_write_end_idx > tx_write_start_idx) {
+    // check how much space is available in the serial buffer
+    int available_bytes = Serial.availableForWrite();
 
-  // Write to serial. Be aware that this will block if serial buffer is full.
-  Serial.write(PACKET_START_TOKEN);
-  Serial.write(lowByte(msg_len));
-  Serial.write(highByte(msg_len));
-  Serial.write(buffer, msg_len);
+    if (available_bytes > 0) {
+      // write as much data as possible without blocking
+      int bytesWritten = Serial.write(buffer + bufferStart, min(tx_write_end_idx - tx_write_start_idx, available_bytes));
+
+      // update bufferStart to point to the start of the remaining data
+      bufferStart += bytesWritten;
+    }
+  }
+
+  // check if all the data in the buffer has been transmitted
+  if (bufferStart == bufferEnd) {
+    // reset bufferStart and bufferEnd to 0
+    bufferStart = 0;
+    bufferEnd = 0;
+  }
 }
 
+// Write to serial. Be aware that this will block if serial buffer is full.
+// if (block) {
+//   Serial.write(PACKET_START_TOKEN);
+//   Serial.write(lowByte(msg_len));
+//   Serial.write(highByte(msg_len));
+//   Serial.write(TX_SERIAL_BUFFER, msg_len);
+// } else {
+
+// }
 bool Communication::message_append(const __FlashStringHelper *msg) {
   size_t existing_len = strlen(TX_STATUS_MSG_BUFFER);
   size_t append_len = strlcpy_P(TX_STATUS_MSG_BUFFER + existing_len, (const char *)msg, TX_STATUS_MSG_BUFFER_SIZE - existing_len);
--- controller/src/communication/communication.hpp
+++ controller/src/communication/communication.hpp
@@ -9,17 +9,29 @@ public:
   ReceiveInterface rx_data;
   TransmitInterface tx_data;
 
-  static const size_t TX_STATUS_MSG_BUFFER_SIZE = 256;
+private:
   const char PACKET_START_TOKEN{ '$' };
+  static const size_t TX_STATUS_MSG_BUFFER_SIZE = 256;
   static const size_t RX_SERIAL_BUFFER_SIZE = 512;
   static const size_t TX_SERIAL_BUFFER_SIZE = 1024;
   static const size_t TX_STATUS_MSG_TRUNC_IND_SIZE = 5;
-  const char TX_STATUS_MSG_TRUNC_IND[TX_STATUS_MSG_TRUNC_IND_SIZE]{" ..."};
-
-private:
+  const char TX_STATUS_MSG_TRUNC_IND[TX_STATUS_MSG_TRUNC_IND_SIZE]{ " ..." };
   char TX_STATUS_MSG_BUFFER[TX_STATUS_MSG_BUFFER_SIZE]{ 0 };
+  char RX_SERIAL_BUFFER[RX_SERIAL_BUFFER_SIZE]{ 0 };
+  char TX_SERIAL_BUFFER[TX_SERIAL_BUFFER_SIZE]{ 0 };
+  uint16_t tx_write_start_idx = 0;
+  uint16_t tx_write_end_idx = 0;
 
-  void write_packet(JsonDocument &tx_doc);
+  // Build packet from json doc, prepend 3 bytes header (start char (1 byte) + message length information (2 bytes)) in dest.
+  // Returns the length of the packet build by this function.
+  template<size_t N>
+  size_t Communication::build_packet(JsonDocument &tx_doc, char (&dest)[N]) {
+    size_t msg_len = serializeJson(tx_doc, dest + 3, N - 3);
+    dest[0] = PACKET_START_TOKEN;
+    dest[1] = lowByte(msg_len);
+    dest[2] = lowByte(msg_len);
+    return msg_len + 3;
+  }
 
 public:
   Communication();
