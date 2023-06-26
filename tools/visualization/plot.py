aea1b5faffa18df3bb94f0f598b69856a1e2973b
Robin Müller <robin-m-99@gmx.de>
2023-06-26 21:14:11 +0200

    Support plotting of single graph

--- tools/visualization/plot.py
+++ tools/visualization/plot.py
@@ -5,17 +5,17 @@ from . import plt
 
 class TimeSeriesPlot:
     def __init__(self, title: str = None, **data: list[tuple[str, pd.DataFrame]]):
-        self._fig, self._ax = plt.subplots(len(data), 1, sharex='all')
+        self._fig, self._ax = plt.subplots(len(data), 1, sharex='all', squeeze=False)
         for index, (label, timeseries) in enumerate(data.items()):
             for name, df in timeseries:
-                self._ax[index].plot(df.iloc[:, 0] - df.iloc[:, 0].min(), df.iloc[:, 1], label=name)
-            self._ax[index].set_ylabel(label)
-            self._ax[index].grid(True)
+                self._ax[index, 0].plot(df.iloc[:, 0] - df.iloc[:, 0].min(), df.iloc[:, 1], label=name)
+            self._ax[index, 0].set_ylabel(label)
+            self._ax[index, 0].grid(True)
 
             # Shrink xaxis by 20 % and fit legend beside
-            box = self._ax[index].get_position()
-            self._ax[index].set_position([box.x0, box.y0, box.width * 0.8, box.height])
-            self._ax[index].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            box = self._ax[index, 0].get_position()
+            self._ax[index, 0].set_position([box.x0, box.y0, box.width * 0.8, box.height])
+            self._ax[index, 0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
         self._fig.supxlabel("Time in seconds")
         if title:
